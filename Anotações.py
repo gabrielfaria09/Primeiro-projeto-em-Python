572eccec470513acbe933016046d2aff93a2c733
gabrielfaria09 <gabrielfaria0905@gmail.com>
2026-05-15 11:13:06 -0300

    Anotações do módulo 2 sobre variáveis

---
+++ Anotações.py
@@ -0,0 +1,11 @@
+#Tipos de variáveis simples
+
+idade = 5 #variável do tipo variável é expressa em minúsculo
+IDADE = 10 #variável do tipo constante é expresa em maiúsculo
+decimais = 3.11 #variáveis também podem ser decimais, mas a vírgula é um ponto
+PYTHON = True #variáveis podem ser booleanas, ou seja, terem valor verdadeiro ou falso
+
+#Tipos de variáveis compostas
+
+lista = [1,2,3] #é possível criar listas de variáveis entre colchetes
+saudacao = "Bom dia" #é possível criar variáveis de string, que exibem letras
